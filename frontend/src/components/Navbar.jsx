1e33c9edaf22514d14e224d6bbd03ceb0a5145b6
Piyush Kumar <955piyush@gmail.com>
2025-09-07 15:12:47 +0530

    completed collection pages and search bar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { assets } from '../assets/assets.js'
 import { Link, NavLink } from 'react-router-dom'
-import { useState } from 'react'
+import { useState ,useContext} from 'react'
+import { ShopContext } from '../context/ShopContext.jsx'
 const Navbar = () => {
    const [visible, setVisible] = useState(false);
+   const {setShowSearch} = useContext(ShopContext);
 
   return (
     <div className="flex items-center  justify-between py-5 font-medium">
@@ -32,7 +34,7 @@ const Navbar = () => {
       </ul>
       <div className="flex items-center gap-6">
         {/* Search icon */}
-        <img src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
+        <img onClick={() => setShowSearch(true)} src={assets.search_icon} className="w-5 cursor-pointer" alt="" />
 
         {/* Profile dropdown */}
         <div className="relative group">
