fcf9893735d8a297b15108dd8ba832a0331526c4
Piyush Kumar <955piyush@gmail.com>
2025-09-23 23:18:13 +0530

    admin panel

---
+++ admin/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './components/Navbar.jsx'
+import Sidebar from './components/Sidebar.jsx'
+import { Routes, Route } from 'react-router-dom'
+import Add from './pages/Add.jsx'
+import List from './pages/List.jsx'
+import Orders from './pages/Orders.jsx'
+import Login from './components/Login.jsx'
+
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const App = () => {
+   
+  const [token, setToken] = React.useState(' ');
+  return (
+    <div className=" bg-gray-50 min-h-screen">
+      {token === ' ' ? <Login setToken={setToken} /> :
+        <>
+          <Navbar />
+          <hr />
+          <div className='flex w-full'>
+            <Sidebar />
+            <div className='w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element={<Add />} />
+                <Route path='/list' element={<List />} />
+                <Route path='/orders' element={<Orders />} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      }
+    </div>
+  )
+}
+
+export default App
